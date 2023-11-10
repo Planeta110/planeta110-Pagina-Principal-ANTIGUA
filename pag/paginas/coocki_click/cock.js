a1e0bd438c624d89af1bb4f23a02f2df39a7a380
Planeta110 <143905008+Planeta110@users.noreply.github.com>
2023-11-10 23:29:51 +0100

    ACTUALIZACION COOCKI CLIQUER
    Se a actualizado y se guarda los datos y se puede reiniciar

--- pag/paginas/coocki_click/cock.js
+++ pag/paginas/coocki_click/cock.js
@@ -1,15 +1,38 @@
-let contador = 0;
+let contador = parseInt(localStorage.getItem("contador")) || 0;
+let cursorr = parseInt(localStorage.getItem("curosr")) || 0;
+let abuelas = parseInt(localStorage.getItem("abuela")) || 0;
+let supercursor = parseInt(localStorage.getItem("supercursor")) || 0;
 const resultadoElement = document.getElementById("resultado");
-let cursorr = 0;
 let segundosAesperarCur = 1000;
-let abuelas = 0;
-let supercursor =0;
 
 function mas1() {
     contador += 1;
     resultadoElement.textContent = "Galletas: " + contador;
 }
 
+if (cursorr => 1) {
+  aumentarContador()
+}
+if (abuelas => 1) {
+  aumentarContadorABU()
+}
+if (supercursor => 1) {
+  aumentarContadorSPC()
+}
+
+function reiniciar() {
+  contador = 0;
+  cursorr = 0;
+  abuelas = 0;
+  supercursor = 0;
+
+  localStorage.setItem("contador", contador.toString());
+  localStorage.setItem("curosr", cursorr.toString());
+  localStorage.setItem("abuela", abuelas.toString());
+  localStorage.setItem("supercursor", supercursor.toString());
+
+  resultadoElement.textContent = "Galletas: " + contador;
+}
 
 function cursor() {
   if (contador >= 50) {
@@ -71,3 +94,62 @@ function aumentarContadorSPC() {
   }, 35000);
   
 }
+
+
+// function cargarDesdeLocalStorage() {
+//   contador = parseInt(localStorage.getItem('galletas'), 10) || 0;
+//   cursorr = parseInt(localStorage.getItem('cursores'), 10) || 0;
+//   abuelas = parseInt(localStorage.getItem('abuelas'), 10) || 0;
+//   supercursor = parseInt(localStorage.getItem('supercursores'), 10) || 0;
+
+//   // Luego de cargar los datos, puedes iniciar el bucle para guardar datos cada 10 segundos
+//   bucleInfinito();
+// }
+
+// // Llama a la función para cargar los datos al cargar la página
+// cargarDesdeLocalStorage();
+
+// Función para guardar datos en localStorage cada 10 segundos
+// function bucleInfinito() {
+//   localStorage.setItem('galletas', contador);
+//   localStorage.setItem('cursores', cursorr);
+//   localStorage.setItem('abuelas', abuelas);
+//   localStorage.setItem('supercursores', supercursor);
+//   setTimeout(bucleInfinito, 10000); // Guardar datos cada 10 segundos
+// }
+function bucleConSetTimeout() {
+    guardar_localstorage()
+    setTimeout(bucleConSetTimeout, 3000); // Llama a la función nuevamente después de 1 segundo (1000 milisegundos)
+}
+
+
+bucleConSetTimeout()
+
+
+function guardar_localstorage () {
+  if(localStorage.setItem("contador", contador),localStorage.setItem("curosr", cursorr),
+    localStorage.setItem("abuela", abuelas),
+    localStorage.setItem("supercursor", supercursor)){
+      localStorage.setItem("contador", contador.toString());
+      localStorage.setItem("curosr", cursorr.toString());
+      localStorage.setItem("abuela", abuelas.toString());
+      localStorage.setItem("supercursor", supercursor.toString());
+    
+  } else{
+    let abuelas = 0;
+    let supercursor =0;
+    let cursorr = 0;
+    let contador = 0;
+    obtener_localstorage()
+  }
+}
+
+function obtener_localstorage() {
+  let contador = parseInt(localStorage.getItem("contador")) || 0;
+  let cursorr = parseInt(localStorage.getItem("curosr")) || 0;
+  let abuelas = parseInt(localStorage.getItem("abuela")) || 0;
+  let supercursor = parseInt(localStorage.getItem("supercursor")) || 0;
+  resultadoElement.textContent = "Galletas: " + contador;
+  
+}
+
